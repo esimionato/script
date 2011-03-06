caecf00ca2053cdd3cd4312bc574b1d68619df09
Cédric BETHENCOURT <cedric.bethencourt@gmail.com>
2011-03-06 12:28:44 +0100

    [install-env-dev-php.sh] add git, vim, sqlite and xdebug; [README] renamed, add new script features

--- install-env-dev-php.sh
+++ install-env-dev-php.sh
@@ -7,36 +7,30 @@ sudo apt-get install -y apache2
 
 echo '------installing php5------'
 sudo apt-get install -y php5
-sudo apt-get install -y php5-dev
 sudo apt-get install -y php5-cli
 sudo apt-get install -y php-apc
 sudo apt-get install -y php5-intl
 
-echo '------installing postgresql------'
+echo '------installing databases------'
 sudo apt-get install -y postgresql
 sudo apt-get install -y php5-pgsql
 sudo apt-get install -y libapache2-mod-php5
+sudo apt-get install -y php5-sqlite
+
+echo '------installing debug tool------'
+sudo apt-get install -y php5-xdebug
 
 echo '------installing css tidy------'
 sudo apt-get install -y tidy
-#tidy -utf8 -i -c -b -q -w 0 -asxhtml filename.html
 sudo apt-get install -y csstidy
-#csstidy main.css --lowercase_s=true --sort_properties=true --template=highest main.min.css
 sudo apt-get install -y yui-compressor
-#yui-compressor --type css main.css -o main.min.css
-#yui-compressor --type js script.js -o script.min.js
-sudo apt-get install -y firebug
-
-#echo '------installing git------'
-#sudo apt-get install -y git-core
-
-echo '------installing svn------'
 sudo apt-get install -y subversion
+sudo apt-get install -y git-core
 
 echo '------installing tree------'
 sudo apt-get install -y tree
 
-echo '------installing gedit------'
+echo '------installing text editors------'
 # Gedit Plugins
 # PPA source repository:
 sudo apt-add-repository ppa:ubuntu-on-rails/ppa
@@ -45,10 +39,11 @@ sudo apt-get update
 sudo apt-get install -y gedit-gmate
 sudo apt-get install -y gedit-plugins
 
+sudo apt-get install -y vim
+
 echo '------updating php5 configuration------'
 sudo a2enmod php5
 sudo a2enmod rewrite
-#sudo chown cedricus:cedricus -R /var/www
 
 echo '------installing firefox extension------'
 sudo apt-get install -y firebug
